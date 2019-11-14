2e948a10e9f476060995d643ea33a751ceb23fdf
bernard-ng <ngandubernard@gmail.com>
2019-11-15 00:51:05 +0200

    #17 add disable state

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,12 +1,24 @@
 import React from "react";
 import classes from './BuildControl.css'
+import PropTypes from 'prop-types';
 
 const buildControl = (props) => (
     <div className={classes.BuildControl}>
         <div className={classes.Label}>{props.label}</div>
-        <button className={classes.Less}>Less</button>
-        <button className={classes.More}>More</button>
+        <button
+            onClick={props.removed}
+            className={classes.Less}
+            disabled={props.disabled}>
+            Less
+        </button>
+        <button onClick={props.added} className={classes.More}>More</button>
     </div>
 );
+
+buildControl.propTypes = {
+    label: PropTypes.string.isRequired,
+    added: PropTypes.func.isRequired,
+    removed: PropTypes.func.isRequired
+};
 
 export default buildControl;
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import classes from 'BuildControls.css';
+import classes from './BuildControls.css';
+import BuildControl from '../BuildControls/BuildControl/BuildControl';
+import PropTypes from 'prop-types';
+
+const controls = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Meat', type: 'meat'},
+    {label: 'Cheese', type: 'cheese'}
+];
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
-
+        {controls.map(c => (
+            <BuildControl
+                key={c.label}
+                label={c.label}
+                added={() => props.ingredientAdded(c.type)}
+                removed={() => props.ingredientRemoved(c.type)}
+                disabled={props.disabled[c.type]}
+            />
+        ))}
     </div>
 );
+
+buildControls.propTypes = {
+    ingredientAdded: PropTypes.func.isRequired,
+    ingredientRemoved: PropTypes.func.isRequired
+};
 
 export default buildControls;
